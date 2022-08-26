0e74f1973cb265a2bb98dcb5b17a51207bd603b4
thadeus <thadeusnyariki94@gmail.com>
2022-08-26 23:31:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (i=0; i<array.length-1; i++){
+    for (j=(i + 1); j<array.length; j++){
+      if (array[i] + array[j] == target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
+// hasTargetSum([3, 8, 12, 4, 11, 7], 10);
 /* 
   Write the Big O time complexity of your function here
 */
@@ -12,6 +20,8 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+// Javascript program to check if there exists a pair
+// in array whose sum results in target.
 */
 
 // You can run `node index.js` to view these console logs
